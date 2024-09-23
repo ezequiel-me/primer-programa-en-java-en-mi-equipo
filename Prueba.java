c030ab84d9c6e642d358c4784adedf220be24942
Melvin <ezequielmaartinez16@gmail.com>
2024-09-23 13:15:34 +0200

    El programa imprime por pantalla mi edad, mi fecha nacimiento, mi comida favorita y mi cantante favorito

--- Prueba.java
+++ Prueba.java
@@ -2,8 +2,8 @@ public class Prueba{
 
 	public static void main(String args[]) {
 
-		System.out.println("Pérez Martínez");
 		System.out.println("19. 2005");
 		System.out.println("Arroz");
+		System.out.println("T.Y.S");
 	}	
 }
